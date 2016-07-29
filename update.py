1c583cca3b9101d31be5ac491cb7da4a1dd40a7a
jizhi <huangqizhi@bao.ac.cn>
2016-07-29 09:56:38 +0000

    2016/07/29 AM 09:56:38

--- update.py
+++ update.py
@@ -4,8 +4,23 @@ import sys
 import time
 
 
+def ShellCmd( cmd ) : 
+	strlist = os.popen(cmd).readlines()
+	for i in xrange(len(strlist)): strlist[i] = strlist[i][:-1]
+	return strlist
+
+
+# deleted
+status = ShellCmd('git status')
+delete = []
+for i in xrange(len(status)) : 
+	if (status[i][1:9] == 'deleted:') : 
+		delete = status[i][9:].split(' ')[-1]
+		os.system('git rm '+delete)
+	
+
 os.system('git status')
-print '--------------------------------------------------'
+print '-------------------------------------------------------'
 print
 print 'Do you really want to add and commit all of these?'
 print 'Press Y to yes, others to no.'
@@ -17,6 +32,7 @@ if (a.lower() in ['y', 'yes']) :
 	mdtime=time.strftime('%Y/%m/%d %p %I:%M:%S', time.localtime())
 	os.system('git commit -m "'+mdtime+'"')
 	os.system('git push -u origin master')
+else : print 'Cancel'
 
 
 
