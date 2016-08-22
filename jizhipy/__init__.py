7aa4977362aaf76c5b9354a376ddc55e5c16821c
jizhi <huangqizhi@bao.ac.cn>
2016-08-22 18:01:28 +0000

    Updated at 2016/08/22 PM 06:01:28

--- jizhipy/__init__.py
+++ jizhipy/__init__.py
@@ -7,12 +7,14 @@ from jizhipy.ConjugateSymmetrize import ConjugateSymmetrize
 from jizhipy.CoordTrans import CoordTrans, CoordTransHealpix
 from jizhipy.DirStr import DirStr
 from jizhipy.Edge2Center import Edge2Center, Center2Edge
+from jizhipy.FAST21cm import FAST21cm
 from jizhipy.FuncFit import Leastsq, FuncFit
 from jizhipy.GSM import GSM
 from jizhipy.Invalid import Invalid
 from jizhipy.IsType import IsType
 from jizhipy.Jy2K import Jy2K
 from jizhipy.Log import *      #@
+from jizhipy.npfmt import npfmt
 from jizhipy.PoolFor import NprocessCPU, PoolFor
 from jizhipy.ProbabilityDensity import BinsArcsinh, ProbabilityDensity
 from jizhipy.ProgressBar import ProgressBar
@@ -28,6 +30,4 @@ from jizhipy.StrFind import StrFind
 from jizhipy.StrlistAdd import StrlistAdd
 from jizhipy.SysMemory import SysMemory
 from jizhipy.Time import Time
-from jizhipy.fast21cm import fast21cm
-from jizhipy.npfmt import npfmt
 
